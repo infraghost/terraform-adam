6cab48c1dc9b00038a1bf3547baec85a4c819c16
hemasivakishore <hemasivakishore@gmail.com>
2025-06-30 17:58:39 +0530

    provider updated

--- 01.main.tf
+++ 01.main.tf
@@ -1,4 +1,12 @@
 # Specify the docker host
+terraform {
+  required_providers {
+    docker = {
+      source  = "kreuzwerker/docker"
+      version = "~3.6.2"
+    }
+  }
+}
 provider "docker" {
   host = "unix:///var/run/docker.sock"
 }
