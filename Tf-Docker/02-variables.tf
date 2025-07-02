3078e38ca56b1b1fbbaa295c8be7cb5dfca7ec2a
hemasivakishore <hemasivakishore@gmail.com>
2025-07-02 19:06:56 +0530

    `Refactor variable "image_name" from list to map and update default value of variable "index"`

--- Tf-Docker/02-variables.tf
+++ Tf-Docker/02-variables.tf
@@ -1,7 +1,15 @@
+# variable "image_name" {
+#   type        = list(any)
+#   description = "Image for Container"
+#   default     = ["nginx:latest", "httpd:latest"]
+# }
+
 variable "image_name" {
-  type        = list(any)
-  description = "Image for Container"
-  default     = ["nginx:latest", "httpd:latest"]
+  type = map(any)
+  default = {
+    "nginx" = "nginx:latest"
+    "httpd" = "httpd:latest"
+  }
 }
 
 variable "container_name" {
@@ -9,8 +17,11 @@ variable "container_name" {
 }
 
 variable "index" {
-  type        = number
-  description = "Index of the image to use from the list"
-  default     = 0
-
+  default = "nginx" # Default to nginx image
 }
+# variable "index" {
+#   type        = number
+#   description = "Index of the image to use from the list"
+#   default     = 0
+
+# }
