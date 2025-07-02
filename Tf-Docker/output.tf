53b9dd23d891e1c0a9d3f048af3db2cca038c532
hemasivakishore <hemasivakishore@gmail.com>
2025-07-02 19:31:51 +0530

    `Removed commented out output values in Tf-Docker/output.tf`

--- Tf-Docker/output.tf
+++ Tf-Docker/output.tf
@@ -1,18 +1,18 @@
-output "ip_address" {
-  value       = docker_container.nginx.ip_address
-  description = "value of the IP address of the container"
-}
+# output "ip_address" {
+#   value       = docker_container.nginx.ip_address
+#   description = "value of the IP address of the container"
+# }
 
-output "container_name" {
-  value       = docker_container.nginx.name
-  description = "value of the name of the container"
-}
+# output "container_name" {
+#   value       = docker_container.nginx.name
+#   description = "value of the name of the container"
+# }
 
-output "container_id" {
-  value       = docker_container.nginx.id
-  description = "value of the ID of the container"
-}
-output "image_name" {
-  value       = docker_image.nginx.name
-  description = "value of the name of the image"
-}
+# output "container_id" {
+#   value       = docker_container.nginx.id
+#   description = "value of the ID of the container"
+# }
+# output "image_name" {
+#   value       = docker_image.nginx.name
+#   description = "value of the name of the image"
+# }
